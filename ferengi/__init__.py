93284eab364f638b732382290c24c6b6832897bb
Richard Neumann <r.neumann@homeinfo.de>
2014-07-04 10:13:14 +0200

    Added pydoc

---
+++ ferengi/__init__.py
@@ -0,0 +1,9 @@
+"""
+Frankly Everything, but Real Estates Notoriously Greedy Importer
+
+System for importing of non-real estate digital signage data.
+"""
+__version__ = "0.1-indev"
+__date__ = "04.07.2014"
+__author__ = "Richard Neumann <r.neumann@homeinfo.de>"
+__credits__ = "To Gene Roddenberry for Star Trek."
