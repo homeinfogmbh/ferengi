ac8703238c52911897b0207bfc89d1f7b9955392
Richard Neumann <r.neumann@homeinfo.de>
2015-04-07 14:34:57 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@
 from distutils.core import setup
 
 setup(
-	name='F.E.R.E.N.G.I.',
-	version='0.1-indev',
-	author='Richard Neumann',
-	author_email='mail@richard-neumann.de',
+    name='ferengi',
+    version='0.1-indev',
+    author='Richard Neumann',
+    author_email='r.neumann@homeinfo.de',
     install_requires=['homie'],
-	packages=['ferengi'],
-	data_files=[],
-	license=open('LICENSE.txt').read(),
-	description='Frankly Everything, but Real Estates Notoriously Greedy Importer',
-	long_description=open('README.txt').read(),
-)
+    packages=['ferengi'],
+    data_files=[],
+    license=open('LICENSE.txt').read(),
+    description=('Frankly Everything, but Real Estates'
+                 ' Notoriously Greedy Importer'),
+    long_description=open('README.txt').read(),
+    )
