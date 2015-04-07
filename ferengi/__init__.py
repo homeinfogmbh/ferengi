232ab48eefb9d9a6b23da7b8579ee450e0164414
Richard Neumann <r.neumann@homeinfo.de>
2015-04-07 14:33:09 +0200

    Fixed docstring and metadata

--- ferengi/__init__.py
+++ ferengi/__init__.py
@@ -2,8 +2,28 @@
 Frankly Everything, but Real Estates Notoriously Greedy Importer
 
 System for importing of non-real estate digital signage data.
+
+          __,----.____
+      _,-' ___// _,','(=
+    ,'\        /,' / ~
+  ,'\        /_/  /           ____
+ /-        /_ /--|        ,--'   ,`-
+/-        /__/ []|_______/     ,'- /
+|         /  |  /.........\  ,'-,-|
+|]      _|,---,  __________ |--|  |
+|  []  [__    |] __________ |--|  |
+|]       |`---'  .........  |--|  |
+|         \__|   \_______ /  `.-`-|
+\-        \ _\ []|       \     `.- \
+ \-        \ _\--|        `--.____,-
+  `./        \_\  \
+    `./    ____\`. \ _
+       `-.__  \\  `_`,(=
+            `----'~
 """
-__version__ = "0.1-indev"
-__date__ = "04.07.2014"
-__author__ = "Richard Neumann <r.neumann@homeinfo.de>"
-__credits__ = "To Gene Roddenberry for Star Trek."
+__version__ = '0.1-indev'
+__date__ = '04.07.2014'
+__author__ = 'Richard Neumann <r.neumann@homeinfo.de>'
+__credits__ = ('* To Gene Roddenberry for Star Trek.\n'
+               '* To Joshua Bell <https://www.blogger.com/profile'
+               '/16877590477046380873> for the FERENGI ascii logo')
