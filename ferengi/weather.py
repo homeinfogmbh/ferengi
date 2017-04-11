cb846f8485e58355e2f2bffa7a0bff08044d53d8
Richard Neumann <r.neumann@homeinfo.de>
2017-04-11 17:00:22 +0200

    Made client more dynamic and defaulting to german now

--- ferengi/weather.py
+++ ferengi/weather.py
@@ -283,15 +283,17 @@ class Weather(_WeatherModel):
 class Client():
     """Receive and store weather data"""
 
-    def __init__(self, base_url=None, api_key=None):
+    def __init__(self, base_url=None, api_key=None, **params):
         """Sets base URL and API key"""
         self.base_url = base_url or self.config['base_url']
         self.api_key = api_key or self.config['api_key']
+        self.params = params
 
     def __call__(self, city_id, raw=False):
         """Retrievels weather data for the respective city ID"""
-        params = {'id': city_id, 'appid': self.api_key, 'units': 'metric'}
-        response = get(self.base_url, params=params)
+        self.params.update(
+            {'id': city_id, 'appid': self.api_key})
+        response = get(self.base_url, params=self.params)
 
         if raw:
             return response
@@ -306,4 +308,4 @@ class Client():
         return config['api']
 
 
-client = Client()   # Default client
+client = Client(units='metric', lang='de')   # Default client
