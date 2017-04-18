40cb645b07bc90b52c750090cb4045ad2b980176
Richard Neumann <r.neumann@homeinfo.de>
2017-04-18 15:11:42 +0200

    Implementing wetter.com API

--- ferengi/wettercom.py
+++ ferengi/wettercom.py
@@ -1,12 +1,12 @@
 """Wetter.com weather data import"""
 
 from datetime import datetime, timedelta
-from contextlib import suppress
 from json import loads
+from hashlib import md5
 
 from requests import get
 from peewee import Model, PrimaryKeyField, ForeignKeyField, BooleanField, \
-    SmallIntegerField, CharField, DateTimeField, DecimalField, FloatField
+    SmallIntegerField, CharField, DateTimeField, DecimalField, IntegerField
 from peeweeplus import dec2dict
 
 from configparserplus import ConfigParserPlus
@@ -14,11 +14,36 @@ from configparserplus import ConfigParserPlus
 from .api import ferengi_database
 
 
+__all__ = ['City', 'Forecast', 'Client', 'client']
+
 config = ConfigParserPlus('/etc/ferengi.d/wettercom.conf')
 database = ferengi_database(
     config['db']['database'],
     user=config['db']['user'],
     passwd=config['db']['passwd'])
+
+
+class UpToDate(Exception):
+    """Indicates that the record is up to date"""
+
+    pass
+
+
+class APIError(Exception):
+    """Indicates that data could not be received from the API"""
+
+    def __init__(self, response):
+        """Sets status code and response text"""
+        super().__init__(response.text)
+        self.response = response
+
+    def __int__(self):
+        """Returns the status code"""
+        return self.response.status_code
+
+    def __str__(self):
+        """Returns the response text"""
+        return self.response.text
 
 
 class _WeatherModel(Model):
@@ -35,9 +60,10 @@ class City(_WeatherModel):
     """Available regions"""
 
     name = CharField(255)
-    country = CharField(2)
-    longitude = FloatField()
-    latitude = FloatField()
+    post_code = CharField(255)
+    region_code = CharField(2)
+    url = CharField(255)
+    country = CharField(255)
     last_update = DateTimeField(null=True, default=None)
     auto_update = BooleanField(default=False)
 
@@ -45,18 +71,12 @@ class City(_WeatherModel):
     def from_dict(cls, dictionary):
         """Creates a city from a dictionary"""
         city = cls()
-        city.id = dictionary['_id']
         city.name = dictionary['name']
+        city.post_code = dictionary['post_code']
+        city.region_code = dictionary['region_code']
+        city.url = dictionary['url']
         city.country = dictionary['country']
-        city.longitude = dictionary['coord']['lon']
-        city.latitude = dictionary['coord']['lat']
         return city
-
-    @classmethod
-    def initialize(cls, list_):
-        """Initializes table from dictionary list"""
-        for dictionary in list_:
-            cls.from_dict(dictionary).save()
 
     @classmethod
     def update_all(cls, force=False):
@@ -93,174 +113,91 @@ class City(_WeatherModel):
 class Forecast(_WeatherModel):
     """Regional weather forecast"""
 
+    SUB_FORECASTS = ('06:00', '11:00', '17:00', '23:00')
+
     city = ForeignKeyField(City, db_column='city')
-    dt = DateTimeField()
-    temp = SmallIntegerField(null=True, default=None)
-    temp_min = SmallIntegerField(null=True, default=None)
-    temp_max = SmallIntegerField(null=True, default=None)
-    pressure = SmallIntegerField(null=True, default=None)
-    sea_level = SmallIntegerField(null=True, default=None)
-    grnd_level = SmallIntegerField(null=True, default=None)
-    humidity = SmallIntegerField(null=True, default=None)
-    clouds_all = SmallIntegerField(null=True, default=None)
-    wind_speed = DecimalField(4, 2, null=True, default=None)
-    wind_deg = DecimalField(6, 3, null=True, default=None)
-    rain_3h = DecimalField(6, 3, null=True, default=None)
-    snow_3h = DecimalField(6, 3, null=True, default=None)
+    parent = ForeignKeyField(
+        'self', db_column='parent', null=True, default=None)
+    timestamp = CharField(255)
+    p = SmallIntegerField()
+    wd = SmallIntegerField()
+    w = SmallIntegerField()
+    tx = SmallIntegerField()
+    pc = SmallIntegerField()
+    tp = DecimalField(4, 2)
+    tn = SmallIntegerField()
+    ws = SmallIntegerField()
+    d = IntegerField()
 
     @classmethod
-    def from_dict(cls, city, dictionary):
+    def _from_dict(cls, city, timestamp, dictionary, parent=None):
         """Creates a forecast for the respective
         city from the specified dictionary
         """
         forecast = cls()
         forecast.city = city
-        forecast.dt = datetime.fromtimestamp(dictionary['dt'])
+        forecast.parent = parent
+        forecast.timestamp = timestamp
+        forecast.p = dictionary['p']
+        forecast.wd = dictionary['wd']
+        forecast.w = dictionary['w']
+        forecast.tx = dictionary['tx']
+        forecast.pc = dictionary['pc']
+        forecast.tp = dictionary['tp']
+        forecast.tn = dictionary['tn']
+        forecast.ws = dictionary['ws']
+        forecast.d = dictionary['d']
+        return forecast
 
-        with suppress(KeyError):
-            forecast.temp = dictionary['main']['temp']
-
-        with suppress(KeyError):
-            forecast.temp_min = dictionary['main']['temp_min']
-
-        with suppress(KeyError):
-            forecast.temp_max = dictionary['main']['temp_max']
-
-        with suppress(KeyError):
-            forecast.pressure = dictionary['main']['pressure']
-
-        with suppress(KeyError):
-            forecast.sea_level = dictionary['main']['sea_level']
-
-        with suppress(KeyError):
-            forecast.grnd_level = dictionary['main']['grnd_level']
-
-        with suppress(KeyError):
-            forecast.humidity = dictionary['main']['humidity']
-
-        with suppress(KeyError):
-            forecast.clouds_all = dictionary['clouds']['all']
-
-        with suppress(KeyError):
-            forecast.wind_speed = dictionary['wind']['speed']
-
-        with suppress(KeyError):
-            forecast.wind_deg = dictionary['wind']['deg']
-
-        with suppress(KeyError):
-            forecast.rain_3h = dictionary['rain']['3h']
-
-        with suppress(KeyError):
-            forecast.snow_3h = dictionary['snow']['3h']
-
+    @classmethod
+    def from_dict(cls, city, timestamp, dictionary):
+        """Creates a forecast for the respective
+        city from the specified dictionary
+        """
+        forecast = cls._from_dict(city, timestamp, dictionary)
         yield forecast
 
-        for weather in dictionary['weather']:
-            yield Weather.from_dict(forecast, weather)
+        for sub_forecast in cls.SUB_FORECASTS:
+            try:
+                d = forecast[sub_forecast]
+            except KeyError:
+                pass
+            else:
+                yield cls._from_dict(city, timestamp, d, parent=forecast)
 
     def to_dict(self):
         """Converts the forecast into a JSON-compliant dictionary"""
-        dictionary = {'dt': self.dt.isoformat()}
-        main = {}
+        dictionary = {
+            'p': self.p,
+            'wd': self.wd,
+            'w': self.w,
+            'tx': self.tx,
+            'pc': self.pc,
+            'tp': dec2dict(self.tp),
+            'tn': self.tn,
+            'ws': self.ws,
+            'd': self.d}
 
-        if self.temp is not None:
-            main['temp'] = self.temp
-
-        if self.temp_min is not None:
-            main['temp_min'] = self.temp_min
-
-        if self.temp_max is not None:
-            main['temp_max'] = self.temp_max
-
-        if self.pressure is not None:
-            main['pressure'] = self.pressure
-
-        if self.sea_level is not None:
-            main['sea_level'] = self.sea_level
-
-        if self.grnd_level is not None:
-            main['grnd_level'] = self.grnd_level
-
-        if self.humidity is not None:
-            main['humidity'] = self.humidity
-
-        if main:
-            dictionary['main'] = main
-
-        if self.clouds_all is not None:
-            dictionary['clouds'] = {'all': self.clouds_all}
-
-        wind = {}
-
-        if self.wind_speed is not None:
-            wind['speed'] = dec2dict(self.wind_speed)
-
-        if self.wind_deg is not None:
-            wind['deg'] = dec2dict(self.wind_deg)
-
-        if wind:
-            dictionary['wind'] = wind
-
-        if self.rain_3h is not None:
-            dictionary['rain'] = {'3h': dec2dict(self.rain_3h)}
-
-        if self.snow_3h is not None:
-            dictionary['snow'] = {'3h': dec2dict(self.snow_3h)}
-
-        weather = [weather.to_dict() for weather in Weather.select().where(
-            Weather.forecast == self)]
-
-        if weather:
-            dictionary['weather'] = weather
+        if self.parent is None:
+            for child in self.__class__.select().where(
+                    self.__class__.parent == self):
+                dictionary[child.timestamp] = child.to_dict()
 
         return dictionary
-
-
-class Weather(_WeatherModel):
-    """Weather details"""
-
-    forecast = ForeignKeyField(Forecast, db_column='forecast')
-    weather_id = SmallIntegerField()
-    main = CharField(255)
-    description = CharField(255)
-    icon = CharField(255)
-
-    @classmethod
-    def from_dict(cls, forecast, dictionary):
-        """Creates a weather record for the respective
-        forecast from the specified dictionary
-        """
-        weather = cls()
-        weather.forecast = forecast
-        weather.weather_id = dictionary['id']
-        weather.main = dictionary['main']
-        weather.description = dictionary['description']
-        weather.icon = dictionary['icon']
-        return weather
-
-    def to_dict(self):
-        """Converts the weather into a JSON-compliant dictionary"""
-        return {
-            'id': self.weather_id,
-            'main': self.main,
-            'description': self.description,
-            'icon': self.icon}
 
 
 class Client():
     """Receive and store weather data"""
 
-    def __init__(self, base_url=None, api_key=None, **params):
+    def __init__(self, base_url=None, user_name=None, api_key=None):
         """Sets base URL and API key"""
         self.base_url = base_url or self.config['base_url']
         self.api_key = api_key or self.config['api_key']
-        self.params = params
+        self.user_name = user_name or self.config['user_name']
 
     def __call__(self, city_id, raw=False):
         """Retrievels weather data for the respective city ID"""
-        self.params.update(
-            {'id': city_id, 'appid': self.api_key})
-        response = get(self.base_url, params=self.params)
+        response = get(self.base_url.format(city_id, self.checksum(city_id)))
 
         if raw:
             return response
@@ -274,5 +211,10 @@ class Client():
         """Returns the API config section"""
         return config['api']
 
+    def checksum(self, city):
+        """Returns the API checksum"""
+        hashval = self.user_name + self.api_key + city
+        return md5(hashval.encode()).hexdigest()
 
-client = Client(units='metric', lang='de')   # Default client
+
+client = Client()   # Default client
