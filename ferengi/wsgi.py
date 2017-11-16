83de0e782a36b14aad17b55cf2f52b50bd921d55
Richard Neumann <r.neumann@homeinfo.de>
2017-11-16 14:41:53 +0100

    Refactored for route property.

--- ferengi/wsgi.py
+++ ferengi/wsgi.py
@@ -3,13 +3,17 @@
 from datetime import datetime
 
 from peewee import DoesNotExist
-from wsgilib import Error, JSON, RestHandler, Route, Router
+from wsgilib import Error, JSON, RestHandler, Router
 
 from ferengi.openweathermap import City, Forecast
 
 __all__ = ['ROUTER']
 
 
+ROUTER = Router()
+
+
+@ROUTER.route('/weather/<city>')
 class WeatherHandler(RestHandler):
     """Handles weather queries."""
 
@@ -25,6 +29,3 @@ class WeatherHandler(RestHandler):
                     (Forecast.city == city) &
                     (Forecast.dt >= datetime.now()))]
             return JSON(forecasts)
-
-
-ROUTER = Router((Route('/weather/<city>'), WeatherHandler))
