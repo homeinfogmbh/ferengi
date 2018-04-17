a0ab5d89794726f0e3ce70418b17e565f96196e9
Richard Neumann <r.neumann@homeinfo.de>
2018-04-17 15:09:53 +0200

    Added Garbage disposal query API.

--- ferengi/wsgi.py
+++ ferengi/wsgi.py
@@ -3,8 +3,11 @@
 from datetime import datetime
 from json import dumps
 
-from flask import Response, Flask
+from flask import jsonify, Response, Flask
 
+from terminallib import Terminal
+
+from ferengi.garbage_disposal import GarbageDisposal
 from ferengi.openweathermap import City, Forecast
 
 __all__ = ['APPLICATION']
@@ -15,6 +18,7 @@ APPLICATION = Flask('ferengi')
 @APPLICATION.route('/weather/<city>')
 def get_weather(city):
     """Returns the respective weather forecasts."""
+
     try:
         city = City.get(City.name == city)
     except City.DoesNotExist:
@@ -25,3 +29,30 @@ def get_weather(city):
             (Forecast.city == city) &
             (Forecast.dt >= datetime.now()))]
     return Response(dumps(forecasts), mimetype='application/json')
+
+
+@APPLICATION.route('/garbage-disposal/<terminal>')
+def get_garbage_disposal(terminal):
+    """Returns the respective weather forecasts."""
+
+    try:
+        tid, cid = terminal.split('.')
+    except ValueError:
+        return ('Invalig terminal ID.', 400)
+
+    try:
+        terminal = Terminal.by_ids(cid, tid)
+    except Terminal.DoesNotExist:
+        return ('No such terminal.', 404)
+
+    try:
+        address = terminal.location.address
+    except AttributeError:
+        return ('Terminal is not located.', 400)
+
+    try:
+        garbage_disposal = GarbageDisposal.by_address(address)
+    except GarbageDisposal.DoesNotExist:
+        return ('No garbage disposal information available.', 404)
+
+    return jsonify(garbage_disposal.to_dict())
