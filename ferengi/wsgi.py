c399bb00fbdd2531dbb66277ca8c5d977f417a07
Richard Neumann <r.neumann@homeinfo.de>
2018-08-15 12:09:04 +0200

    Fixed add_routes().

--- ferengi/wsgi.py
+++ ferengi/wsgi.py
@@ -11,7 +11,7 @@ __all__ = ['APPLICATION']
 
 
 APPLICATION = Application('ferengi', cors=True)
-APPLICATION.add_routes(openweathermap.ROUTES, garbage_disposal.ROUTES)
+APPLICATION.add_routes(openweathermap.ROUTES + garbage_disposal.ROUTES)
 
 
 @APPLICATION.errorhandler(Exception)
