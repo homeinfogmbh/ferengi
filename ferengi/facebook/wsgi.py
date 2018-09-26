7c174bc46aa9c994a9a952b2cf9a3aa03a674f3e
Richard Neumann <r.neumann@homeinfo.de>
2018-09-26 13:31:33 +0200

    Implemented Image proxying.

--- ferengi/facebook/wsgi.py
+++ ferengi/facebook/wsgi.py
@@ -2,19 +2,20 @@
 
 from datetime import date, timedelta
 
-from flask import request
+from flask import request, Response
+from requests import get
 
-from wsgilib import JSON, XML
+from wsgilib import JSON, XML, Error
 
 from ferengi.facebook.client import FACEBOOK
-from ferengi.facebook.functions import posts_to_dom
+from ferengi.facebook.functions import posts_to_dom, decode_image_url
 
 
 __all__ = ['ROUTES']
 
 
 def get_posts(facebook_id):
-    """Returns the respective weather forecasts."""
+    """Returns Facebook posts."""
 
     days = int(request.args.get('days', 14))
     since = date.today() - timedelta(days=days)
@@ -22,9 +23,24 @@ def get_posts(facebook_id):
     posts = FACEBOOK.get_posts(facebook_id, since=since, limit=limit)
 
     if 'xml' in request.args:
-        return XML(posts_to_dom(posts))
+        proxy = 'proxy' in request.args
+        return XML(posts_to_dom(posts, proxy=proxy))
 
     return JSON([post.to_json(html=True) for post in posts])
 
 
-ROUTES = (('GET', '/facebook/<facebook_id>', get_posts, 'get_facebook_posts'),)
+def get_image(path):
+    """Proxies images from Facebook."""
+
+    url = decode_image_url(request.url)
+    response = get(url)
+
+    if response.status_code == 200:
+        return Response(response.data, mimetype=response.content_type)
+
+    return Error(response.text)
+
+
+ROUTES = (
+    ('GET', '/facebook/<facebook_id>', get_posts, 'get_facebook_posts'),
+    ('GET', '/facebook/image/<path>', get_image, 'get_facebook_image'))
