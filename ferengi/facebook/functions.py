4f93b4c879995445e2a5e063199d471c08f6d8dd
Richard Neumann <r.neumann@homeinfo.de>
2018-09-26 13:30:28 +0200

    Added Image URL decoding / encoding.

--- ferengi/facebook/functions.py
+++ ferengi/facebook/functions.py
@@ -1,11 +1,48 @@
 """Miscellaneous functions."""
 
+from pathlib import Path
+from urllib.parse import urlparse
+
 from ferengi.facebook import dom
 
-__all__ = ['posts_to_dom']
+
+__all__ = ['encode_image_url', 'decode_image_url', 'posts_to_dom']
 
 
-def posts_to_dom(posts):
+SCHEME = 'https'
+HOST = 'ferengi.homeinfo.de'
+IMG_PATH = '/facebook/image'
+
+
+def encode_image_url(url, path=IMG_PATH):
+    """Translates URLSs to be proxied."""
+
+    url = urlparse(url)
+
+    if url.fragment:
+        raise ValueError('Cannot encode URL with fragment.')
+
+    params = ';{}'.format(url.params) if url.params else ''
+    query = '?{}'.format(url.query) if url.query else ''
+    return '{}://{}{}{}{}{}#{}'.format(
+        SCHEME, HOST, path, url.path, params, query, url.netloc)
+
+
+def decode_image_url(url):
+    """Translates URLSs to be proxied."""
+
+    url = urlparse(url)
+    path = Path(url.path)
+    parts = path.parts[2:]
+    path = '/' + '/'.join(parts)
+    host = url.fragment
+    params = ';{}'.format(url.params) if url.params else ''
+    query = '?{}'.format(url.query) if url.query else ''
+    return '{}://{}{}{}{}{}'.format(
+        SCHEME, host, path, url.path, params, query)
+
+
+def posts_to_dom(posts, proxy=False):
     """Converts the given posts to XML dom."""
 
     channel = dom.channel()
@@ -15,7 +52,12 @@ def posts_to_dom(posts):
         content.created_time = post.created
         content.from_ = post.author
         content.message = post.html
-        content.image = post.image
+
+        if proxy:
+            content.image = encode_image_url(post.image)
+        else:
+            content.image = post.image
+
         channel.content_.append(content)
 
     return channel
