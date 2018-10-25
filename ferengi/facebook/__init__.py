7cf410655b058c94c86007c36215cac6a5d3085c
Richard Neumann <r.neumann@homeinfo.de>
2018-10-25 17:10:55 +0200

    Added more exports.

--- ferengi/facebook/__init__.py
+++ ferengi/facebook/__init__.py
@@ -1,6 +1,8 @@
 """Facebook client API."""
 
+from ferengi.facebook.client import Facebook, FACEBOOK
+from ferengi.facebook.functions import posts_to_dom
 from ferengi.facebook.wsgi import ROUTES
 
 
-__all__ = ['ROUTES']
+__all__ = ['FACEBOOK', 'ROUTES', 'posts_to_dom', 'Facebook']
