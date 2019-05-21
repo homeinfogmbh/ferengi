a3f427bf7d9c003ecdfd1c8a11985e291fb79e7b
Richard Neumann <r.neumann@homeinfo.de>
2019-05-21 10:22:48 +0200

    Fix.

--- ferengi/facebook/__init__.py
+++ ferengi/facebook/__init__.py
@@ -1,8 +1,8 @@
 """Facebook client API."""
 
-from ferengi.facebook.client import Facebook
+from ferengi.facebook.client import Facebook, Post
 from ferengi.facebook.functions import posts_to_dom
 from ferengi.facebook.wsgi import ROUTES
 
 
-__all__ = ['ROUTES', 'posts_to_dom', 'Facebook']
+__all__ = ['ROUTES', 'posts_to_dom', 'Facebook', 'Post']
