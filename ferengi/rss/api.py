c5a6e21b834e49e11dd931ad94569e55af49f8a4
Richard Neumann <r.neumann@homeinfo.de>
2020-01-31 16:00:32 +0100

    Added logging.

--- ferengi/rss/api.py
+++ ferengi/rss/api.py
@@ -1,13 +1,19 @@
 """API to be invoked."""
 
+from logging import getLogger
+
 from ferengi.rss.orm import Source
 
 
 __all__ = ['update']
 
 
+LOGGER = getLogger('RSS')
+
+
 def update(delete_old=True):
     """Updates all sources."""
 
     for source in Source:
+        LOGGER.info('Updating source: %s.', source.name)
         source.update_feed(delete_old=delete_old)
