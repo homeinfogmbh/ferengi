c8d01ca5269f35cfc13987356cf799163f4a6296
Richard Neumann <mail@richard-neumann.de>
2020-12-25 00:16:00 +0100

    Refac.

--- ferengi/garbage_disposal/orm.py
+++ ferengi/garbage_disposal/orm.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 from time import sleep
+from typing import Iterable, Iterator
 
 from peewee import BooleanField
 from peewee import CharField
@@ -52,7 +53,7 @@ class Location(_GarbageDisposalModel):
     timestamp = DateTimeField(default=datetime.now)
 
     @classmethod
-    def up2date(cls, address):
+    def up2date(cls, address: Address) -> bool:
         """Determines whether the respective address' data is up to date."""
         garbage_disposals = tuple(cls.by_address(address))
 
@@ -63,12 +64,12 @@ class Location(_GarbageDisposalModel):
                    for garbage_disposal in garbage_disposals)
 
     @classmethod
-    def by_address(cls, address):
+    def by_address(cls, address: Address) -> Iterable[Location]:
         """Returns the respective garbage disposal by address."""
         return cls.select().where(cls.address == address)
 
     @classmethod
-    def refresh(cls, address, force=False):
+    def refresh(cls, address: Address, force: bool = False):
         """Updates the records for the respective address."""
         if force or not cls.up2date(address):
             cls.purge(address)
@@ -77,7 +78,7 @@ class Location(_GarbageDisposalModel):
                 record.save()
 
     @classmethod
-    def refresh_all(cls, addresses, force=False):
+    def refresh_all(cls, addresses: Iterable[Address], force: bool = False):
         """Refreshes all addresses."""
         for address in addresses:
             if address.city not in DISTRICTS:
@@ -92,7 +93,7 @@ class Location(_GarbageDisposalModel):
                 sleep(WAIT_TIME)
 
     @classmethod
-    def update_all(cls, force=False):
+    def update_all(cls, force: bool = False):
         """Updates the garbage disposal for all active systems."""
         addresses = set()
 
@@ -102,39 +103,40 @@ class Location(_GarbageDisposalModel):
         cls.refresh_all(addresses, force=force)
 
     @classmethod
-    def purge(cls, address):
+    def purge(cls, address: Address):
         """Purges records for the respective address."""
         for record in cls.select().where(cls.address == address):
             record.delete_instance()
 
     @classmethod
-    def from_address(cls, address):
+    def from_address(cls, address: Address) -> Iterator[Location]:
         """Creates an entry from the respective address."""
         for pickup_solution in get_disposals(address):
             yield from cls.from_dict(address, pickup_solution.to_json())
 
     @classmethod
-    def from_dict(cls, address, dictionary):
+    def from_dict(cls, address: Address, dct: dict) -> Iterator[Model]:
         """Creates the respective records from the given dictionary."""
         record = cls()
         record.address = address
-        record.code = dictionary['code']
-        record.street = dictionary['street']
-        record.house_number = dictionary['house_number']
-        record.district = dictionary['district']
+        record.code = dct['code']
+        record.street = dct['street']
+        record.house_number = dct['house_number']
+        record.district = dct['district']
         yield record
 
-        for pickup in dictionary['pickups']:
+        for pickup in dct['pickups']:
             yield from Pickup.from_dict(record, pickup)
 
-    def to_json(self):
+    def to_json(self) -> dict:
         """Returns a JSON-ish dictionary."""
         return {
             'code': self.code,
             'street': self.street,
             'house_number': self.house_number,
             'district': self.district,
-            'pickups': [pickup.to_json() for pickup in self.pickups]}
+            'pickups': [pickup.to_json() for pickup in self.pickups]
+        }
 
 
 class Pickup(_GarbageDisposalModel):
@@ -149,27 +151,28 @@ class Pickup(_GarbageDisposalModel):
     interval = CharField(16)
 
     @classmethod
-    def from_dict(cls, location, dictionary):
+    def from_dict(cls, location: Location, dct: dict) -> Iterator[Model]:
         """Creates and yields respective records from the dictionary."""
         record = cls()
         record.location = location
-        record.type_ = dictionary['type']
-        record.image_link = dictionary['image_link']
-        record.weekday = dictionary['weekday']
-        record.interval = dictionary['interval']
+        record.type_ = dct['type']
+        record.image_link = dct['image_link']
+        record.weekday = dct['weekday']
+        record.interval = dct['interval']
         yield record
 
-        for date in dictionary.get('next_dates', ()):
+        for date in dct.get('next_dates', ()):
             yield PickupDate.from_dict(record, date)
 
-    def to_json(self):
+    def to_json(self) -> dict:
         """Returns a JSON-ish dictionary."""
         return {
             'type': self.type_,
             'image_link': self.image_link,
             'weekday': self.weekday,
             'interval': self.interval,
-            'next_dates': [date.to_json() for date in self.next_dates]}
+            'next_dates': [date.to_json() for date in self.next_dates]
+        }
 
 
 class PickupDate(_GarbageDisposalModel):
@@ -183,18 +186,19 @@ class PickupDate(_GarbageDisposalModel):
     exceptional = BooleanField()
 
     @classmethod
-    def from_dict(cls, pickup, dictionary):
+    def from_dict(cls, pickup: Pickup, dct: dict) -> PickupDate:
         """Creates and yields respective records from the dictionary."""
         record = cls()
         record.pickup = pickup
-        record.date = datetime.strptime(dictionary['date'], '%Y-%m-%d')
-        record.weekday = dictionary['weekday']
-        record.exceptional = dictionary['exceptional']
+        record.date = datetime.strptime(dct['date'], '%Y-%m-%d')
+        record.weekday = dct['weekday']
+        record.exceptional = dct['exceptional']
         return record
 
-    def to_json(self):
+    def to_json(self) -> dict:
         """Returns a JSON-ish dictionary."""
         return {
             'date': self.date.strftime('%Y-%m-%d'),
             'weekday': self.weekday,
-            'exceptional': self.exceptional}
+            'exceptional': self.exceptional
+        }
