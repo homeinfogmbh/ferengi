7988d5a886d42cc217d13ff9eebd245fd36d8472
Richard Neumann <mail@richard-neumann.de>
2020-12-25 04:16:20 +0100

    Refac.

--- ferengi/wsgi.py
+++ ferengi/wsgi.py
@@ -11,9 +11,10 @@ __all__ = ['APPLICATION']
 
 
 APPLICATION = Application('ferengi', cors=True)
-APPLICATION.add_routes(
-    facebook.ROUTES + openligadb.ROUTES + openweathermap.ROUTES
-    + garbage_disposal.ROUTES)
+APPLICATION.add_routes(facebook.ROUTES)
+APPLICATION.add_routes(openligadb.ROUTES)
+APPLICATION.add_routes(openweathermap.ROUTES)
+APPLICATION.add_routes(garbage_disposal.ROUTES)
 
 
 @APPLICATION.errorhandler(Exception)
