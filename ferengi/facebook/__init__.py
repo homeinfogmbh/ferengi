505fd6a257086dc8f0fb30cea193271aa8b45fad
Richard Neumann <mail@richard-neumann.de>
2021-08-19 16:26:59 +0200

    remove facebook module

--- ferengi/facebook/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Facebook client API."""
-
-from ferengi.facebook.client import Facebook, Post
-from ferengi.facebook.functions import posts_to_dom
-from ferengi.facebook.wsgi import ROUTES
-
-
-__all__ = ['ROUTES', 'posts_to_dom', 'Facebook', 'Post']
--- ferengi/facebook/config.py
+++
@@ -1,9 +0,0 @@
-"""Facebook configuration."""
-
-from ferengi.config import ferengi_config
-
-
-__all__ = ['CONFIG']
-
-
-CONFIG = ferengi_config('facebook.conf')
