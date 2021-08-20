eadaa6f19c241d3b1f9de8384556d024dbcf8324
Richard Neumann <mail@richard-neumann.de>
2021-08-20 13:16:54 +0200

    Remove facebook remainders

--- ferengi/wsgi.py
+++ ferengi/wsgi.py
@@ -4,14 +4,13 @@ from traceback import format_exc
 
 from wsgilib import Application, Error
 
-from ferengi import facebook, openligadb, openweathermap, garbage_disposal
+from ferengi import openligadb, openweathermap, garbage_disposal
 
 
 __all__ = ['APPLICATION']
 
 
 APPLICATION = Application('ferengi', cors=True)
-APPLICATION.add_routes(facebook.ROUTES)
 APPLICATION.add_routes(openligadb.ROUTES)
 APPLICATION.add_routes(openweathermap.ROUTES)
 APPLICATION.add_routes(garbage_disposal.ROUTES)
