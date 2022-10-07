6190d2f337f6b4c928cdada77a68e34fbc93d75b
Richard Neumann <r.neumann@homeinfo.de>
2022-10-07 14:35:46 +0200

    Add type hints

--- ferengi/openweathermap/client.py
+++ ferengi/openweathermap/client.py
@@ -1,8 +1,9 @@
 """OpenWeatherMap API client."""
 
 from json import loads
+from typing import Any, Optional, Union
 
-from requests import get
+from requests import Response, get
 
 from ferengi.api import APIError
 from ferengi.openweathermap.config import CONFIG
@@ -14,13 +15,22 @@ __all__ = ['CLIENT']
 class Client:
     """Receive and store weather data."""
 
-    def __init__(self, base_url: str = None, api_key=None, **params):
+    def __init__(
+            self,
+            base_url: Optional[str] = None,
+            api_key: Optional[str] = None,
+            **params
+    ):
         """Sets base URL and API key"""
         self.base_url = base_url or self.config['base_url']
         self.api_key = api_key or self.config['api_key']
         self.params = params
 
-    def __call__(self, city_id, raw=False):
+    def __call__(
+            self,
+            city_id: int,
+            raw: bool = False
+    ) -> Union[Response, dict]:
         """Retrievels weather data for the respective city ID."""
         self.params.update({'id': city_id, 'appid': self.api_key})
         response = get(self.base_url, params=self.params)
@@ -34,7 +44,7 @@ class Client:
         raise APIError(response)
 
     @property
-    def config(self):
+    def config(self) -> dict[str, Any]:
         """Returns the API config section."""
         return CONFIG['api']
 
