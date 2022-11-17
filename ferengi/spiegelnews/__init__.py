85cbb8124e36e68cd050e5fa9214f401bc6219a0
Richard Neumann <r.neumann@homeinfo.de>
2022-11-17 12:23:15 +0100

    Export News ORM model

--- ferengi/spiegelnews/__init__.py
+++ ferengi/spiegelnews/__init__.py
@@ -1,6 +1,7 @@
 """News from RSS feed of spiegel.de."""
 
 from ferengi.spiegelnews.client import update
+from ferengi.spiegelnews.orm import News
 
 
-__all__ = ['update']
+__all__ = ['News', 'update']
