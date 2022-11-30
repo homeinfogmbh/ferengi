e98786c68ab22edf2701b317ecf2fc1aa3a28f62
Richard Neumann <r.neumann@homeinfo.de>
2022-11-30 14:59:13 +0100

    Log imported city

--- ferengi/googlenews/client.py
+++ ferengi/googlenews/client.py
@@ -1,5 +1,7 @@
 """News update client."""
 
+from logging import getLogger
+
 from ferengi.googlenews.constants import CITIES
 from ferengi.googlenews.orm import News
 from ferengi.rss import update_from_url
@@ -12,4 +14,5 @@ def update() -> None:
     """Update news from default RSS feed."""
 
     for city, url in CITIES.items():
+        getLogger('Google News').info('Importing news for city: %s', city)
         update_from_url(url, News, city=city)
