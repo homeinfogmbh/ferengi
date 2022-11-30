700f8b80bcde1f06428e286b1963133a959f2ceb
Richard Neumann <r.neumann@homeinfo.de>
2022-11-30 11:58:17 +0100

    Export CITIES

--- ferengi/googlenews/__init__.py
+++ ferengi/googlenews/__init__.py
@@ -1,7 +1,8 @@
 """News from RSS feed from google.com for Hannover."""
 
 from ferengi.googlenews.client import update
+from ferengi.googlenews.constants import CITIES
 from ferengi.googlenews.orm import News
 
 
-__all__ = ['News', 'update']
+__all__ = ['CITIES', 'News', 'update']
